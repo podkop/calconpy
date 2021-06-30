406078e320de3db7b09589a8ecb9d28021042970
podkop <dmitry.podkopaev@gmail.com>
2021-06-30 10:13:47 +0200

    Alpha version

--- cc_main.py
+++ cc_main.py
@@ -7,26 +7,43 @@ CALculations CONtrol
 import sys
 import os
 import json
+from importlib import import_module
+from numpy import base_repr # for compacting hash string
+import hashlib
+from shutil import rmtree
 
 # Constants
-dflt_r_params_fname = "_init_routines"
+temp_step_fname = "_temp_step"
+"""Folder name where step results are stored before renaming to hash folder"""
+step_config_fname = "_config"
+"""file name where step's config is stored"""
+dfl_r_init_fname = "_init_routines"
 """Default name of `.json` file listing routines` parameters"""
-dflt_step_name = "Main"
+dfl_step_name = "Main"
 """Default step name if the sequence consists of a single step"""
 step_prefix = "$"
 """Prepended to a step name when it serves as a config. parameter name"""
+step_prefix_len = len(step_prefix)
 pname_seq = "_sequence"
 """Name of the parameter defining the calculations sequence"""
 pname_invar = "_invariant"
 """Name of parameter defining invariant-caching parameters"""
 iname_cached = "_cached"
 """Name of init. parameter defining which routines are cached"""
-iname_not_cached = "_noncached"
+iname_non_cached = "_noncached"
 """Name of init. parameter defining which routines are NOT cached"""
+sys_params_set = {pname_seq,pname_invar}
+"""Set of system parameter names"""
+sys_params_hashed_set = {pname_seq}
+"""Set of system parameter names which are included in hash"""
+return_stats_key = "_stats"
+"""Key name containing summary stats returned by routines"""
+return_res_key = "_result"
+"""Key name containing output returned by routines"""
 
 def _str_key(arg):
     """
-    Given arg as `str` or `dict`, returns arg or its 1st key 
+    Given arg as `str` or `dict`, returns str := arg or its 1st key 
     """
     if isinstance(arg,str):
         return arg
@@ -34,18 +51,21 @@ def _str_key(arg):
 
 def _list_val(arg):
     """
-    Given arg as `str` or `dict`, returns [] or the value for the 1st key 
+    Given arg as `str` or `dict`, returns list := [] or value of the 1st key 
     """
     if isinstance(arg,str):
         return []
     return next(iter(arg.values()))
 
-# def _remove_ls_pref(l,pref="_"):
-#     """
-#     Given a list of strings, returns the list without strings starting with "_"
-#     """
-#     return [si for si in l if not si.startswith(pref)]
-    
+def _remove_ls_prefix(l,pref="_"):
+    """
+    Given a list-like of strings, returns the list without strings 
+    starting with "_"
+    """
+    return [si for si in l if not si.startswith(pref)]
+def _param2stepname(param):
+    """Given parameter of step's routine, returns the step name"""
+    return param[step_prefix_len:]
 def _fn_normalize(fname,folder,ext):
     """
     Given file name, folder and extension, 
@@ -58,13 +78,35 @@ def _fn_normalize(fname,folder,ext):
         ext = "."+ext
     return os.path.join(folder,fname)+ext
 
-def force_list(arg):
+def _force_list(arg):
     """
     If the argument is not a list, wraps it in the list.
     """
     if isinstance(arg,list):
         return arg
     return [arg]
+
+## Converts the given dictionary to string invariant to keys order
+#   - the dictoinary can be nested and include lists
+#   - all keys at any level should be string
+def _dict2str(dct):
+    return json.dumps(dct,sort_keys=True)
+
+## Calculates unique hash for a string, returns str
+def _str2hash(string):
+    return base_repr(
+        int(
+            hashlib.shake_128(
+                bytearray(string,"utf-8")
+            ).hexdigest(10),
+            16
+        ),
+        base=36
+        )
+## Calculates hash for a given dict
+def _dict2hash(dct):
+    return _str2hash(_dict2str(dct))
+
 
 class calcon:
     """
@@ -80,14 +122,15 @@ class calcon:
         routines_params: dict or str, optional
             * dict: for each routine that can be used in calculations, 
               <routine name>: list of parameters names influencing the routine
-              "_cached": list of cached routines or 
-              "_noncached": list of non-cached routines (by default, all are cached)
+              "_cached": list of cached routine names or 
+              "_noncached": list of non-cached routine names
+                  * "_noncached" works only if "_cached" is missing
+                  * by default, all routines are cached
             * str: name of `json` file containing the above dict, optionally
-              containing path relative to `calc_folder` or absolute
+              containing path relative to `calc_folder` or absolute path
                   * if the extension is missing, it is set to `.json`
-                  * by default, `routines_params` are taken from 
-                    `<calc_folder>/<dflt_r_params_fname>`
-            * list of str: list of routines' names            
+                  * by default, the file is in 
+                    `<calc_folder>/<dfl_r_init_fname>`
         routines_package: str, optional
             Provide <package> name, if each <routine> is implemented as the
             <package>.<routine> module, i.e. routine's methods are 
@@ -111,7 +154,7 @@ class calcon:
     
     def __init__(self,
         calc_folder,
-        routines_params = dflt_r_params_fname,
+        routines_params = dfl_r_init_fname,
         routines_package = None,
         routines_module = "__main__",
         configs_subfolder = ""
@@ -121,45 +164,82 @@ class calcon:
         self._config_folder = os.path.join(
             self._calc_folder,configs_subfolder
             )
-        # Reading routines parameters/list if needed
+        self._temp_folder = os.path.join(calc_folder,temp_step_fname)
+        # Reading routines parameters if needed
         if isinstance(routines_params,str):
             routines_params = self._read_json(routines_params)
-        # Creating hooks for routines, initializing objects if needed
+        # Dictionary of sets of routines' parameters
+        self._r_params = {
+            ki: set(_force_list(vi)) 
+                for ki,vi in routines_params.items() if
+                    not(ki.startswith("_"))
+            }
+        # Set of routines names
+        self._r_names = set( self._r_params.keys() )
+        # Set information about routines caching
+        self._set_r_caching_info(
+            *[
+                routines_params.get(ki,None) 
+                    for ki in [iname_cached,iname_non_cached]
+                ]
+            )
+        # Creating hooks for routines
         if routines_package is not None:
-            # Each routine is a module's function
-            pkg_prefix = routines_package+"." if routines_package else ""
-            self._routines = {
-                ri: sys.modules[pkg_prefix+ri] for ri in routines_params
-                }
+            self._r_modules_hooks(routines_package)
         else:
-            self._routines = {
-                ri: getattr(sys.module[routines_module],ri)()
-                    for ri in routines_params
-                }
-        # Saving information about sets of routines' parameters
-        if isinstance(routines_params,dict):
-            self._r_params = {
-                ki: set(_remove_ls_pref(force_list(vi))) 
-                    for ki,vi in routines_params.items()
-                }
+            self._r_objects_hooks(routines_module)
+
+    # Saving information about if routines are cached
+    def _set_r_caching_info(self,l_cached,l_non_cached):
+        if l_cached is not None:
+            self._r_caching = {ki:False for ki in self._r_names}
+            self._r_caching.update( {ki: True for ki in l_cached} )
         else:
-            self._r_params = {ki: None for ki in routines_params}
-    def _read_json(self,fname):
+            self._r_caching = {ki:True for ki in self._r_names}
+            if l_non_cached is not None:
+                self._r_caching.update( {ki: False for ki in l_non_cached} )
+    # Creating hooks for routines as modules (import if needed)
+    def _r_modules_hooks(self,package_name):
+        pkg_prefix = package_name+"." if package_name else ""
+        self._routines = {
+            ri: sys.modules.get(
+                    pkg_prefix+ri,
+                    import_module(pkg_prefix+ri)
+                    ) 
+                for ri in self._r_names
+            }
+    # Creating hooks for routines as module's objects (import if needed)
+    def _r_objects_hooks(self,module_name):
+        r_module = sys.module.get(
+            module_name,
+            import_module(module_name)
+            )
+        self._routines = {
+            ri: getattr(r_module,ri)()
+                for ri in self._r_names
+            }
+
+    def _read_json(self, fname, subfolder=""):
         """
         Reads a json file and returns it as object (dict/list/etc). 
         `fname` can be just a name or a full file name.
-        By default, it is a `.json` file in `config_folder`.
+        By default, it is a `.json` file in `config_folder/subfolder`.
         """
-        fname = _fn_normalize(fname,self.calc_folder,".json")
+        fname = _fn_normalize(
+            fname,
+            os.path.join(self.calc_folder,subfolder),
+            ".json"
+            )
         with open(fname) as f:
             return json.load(f)
+    
     def _inclusion_list(self,step_name,lst):
         """
         Recursively updates boolean list (which steps from self._seq should be
         included, keeping all dependencies starting from the given step)
         """
-        lst[self._steps_nrs[step_name]] = True
-        for si in self._steps_depend[step_name]:
+        lst[self._s_nrs[step_name]] = True
+        for si in self._s_parent[step_name]:
             self._inclusion_list(si,lst)
     def _subsequence(self,nr):
         """
@@ -175,37 +255,109 @@ class calcon:
         Given step name, returns the set of its routine's parameters
         """
         return getattr(
-            self._r_params, self._steps_routine[step], set()
+            self._r_params, self._s_routine[step], set()
             )
-    def _load_config(self,config):
+    def load_config(self,config):
         """
         Given master configuration as `dict` or `str` (config. file name),
         checks its consistency and prepares calculations plan.
         """
         if isinstance(config,str):
             config = self._read_json(config)
-        #? self._config = config
+        invar_set = set(config.get(pname_invar,[]))
         # The calculations plan (by default, has one step "Main")
-        self._seq = config.get(pname_seq, [dflt_step_name])
+        self._seq = config.get(pname_seq, [dfl_step_name])
         # Dictionaries containing various information for each step
-        self._steps_config = {} # Step's full configuration
-        self._steps_params = {} # Step's param. names including dependencies
-        self._steps_invar = {} # Param. names step's caching is invariant to
-        self._steps_nrs = {} # Nr of step in master sequence
-        self._steps_depend = {} # List of steps the step depends on
-        self._steps_seq = {} # Step's subsequence
-        self._steps_routine = {} # Step's routine name
-        self._steps_cached = {} # If step is cached (boolean)
-        self._steps_cached = {} # Cached folder name if exists
+        self._s_config = {} # Step's full configuration
+        self._s_params = {} # Step's param. names including dependencies
+        self._s_hash_params = {} # Param. names used in step's hashing
+        self._s_nrs = {} # Nr of step in master sequence
+        self._s_parent = {} # List of steps the step depends on
+        self._s_seq = {} # Step's subsequence
+        self._s_routine = {} # Step's routine name
+        self._s_if_cached = {} # If step is cached (boolean)
+        self._s_cached_folder = {} # Cached folder name if exists
         for i,ist in enumerate(self._seq): # ist is calculations step (#i)
             step_name = _str_key(ist)
-            self._steps_nrs[step_name] = i
-            self._steps_depend[step_name] = _list_val(ist)
-            self._steps_seq[step_name] = subseq = self._subsequence(i)
-            self._steps_routine[step_name] = config[step_prefix+step_name]
-            self._steps_params[step_name] = set.union(
+            self._s_nrs[step_name] = i
+            self._s_parent[step_name] = s_parent = _list_val(ist)
+            self._s_seq[step_name] = subseq = self._subsequence(i)
+            self._s_routine[step_name] = config[step_prefix+step_name]
+            self._s_params[step_name] = s_params = set.union(
                 set([step_prefix+step_name]),
-                    *[self._get_params(_str_key(si)) for si in subseq]
+                    *[self._get_params(_str_key(si)) for si in s_parent]
                 )
-            self._steps_invar = 
-            self._steps_config[step_name] = cfg =
+            self._s_invar = s_invar = invar_set.intersect(
+                s_params,sys_params_hashed_set
+                )
+            self._s_hash_params[step_name] = s_params.union(
+                sys_params_hashed_set
+                    ).difference(invar_set)
+            self._s_config[step_name] = s_config = {
+                config[ki] for ki in s_params
+                }
+            self._s_config[step_name][pname_seq] = subseq
+            if len(s_invar) > 0:
+                self._s_config[step_name][pname_invar] = sorted(list(s_invar))
+            self._s_cached_folder[step_name] = _dict2hash(s_config)
+    # Prepares a new temp folder for step calculation with step's config file
+    def _make_step_folder(self,s_name):
+        if os.path.exists(self._temp_folder):
+            rmtree(self._temp_folder)
+        os.mkdir(self._temp_folder)
+        with open(
+            _fn_normalize(step_config_fname,self._temp_folder,"json"),"w"
+                ) as f:
+            json.dump(self.s_config[s_name],f)
+    # After step's calculation ends successfuly, renames the temp folder to
+    # its intended name
+    def _checkin_step_folder(self,s_name):
+        cached = os.path.join(self._calc_folder,self._s_cached_folder[s_name])
+        if os.path.exists(cached):
+            rmtree(cached)
+        os.path.rename(self._temp_folder,cached)
+        
+    def run_step(self,s_name):
+        """
+        Perform calculation of the given step. 
+        Returns False if calculation was successful, 
+        otherwise returns error information (yielding True)
+        """
+        q_cached = self._s_if_cached[s_name]
+        args = [self._s_res[si] for si in self._s_parent]
+        if q_cached:
+            self._make_temp_folder(s_name)
+            args.append(self._temp_folder)
+        args.append(self._s_sonfig[s_name])
+        try:
+            val = self._routine(*args)
+        except:
+            return True
+        if q_cached:
+            self._stats[s_name] = val
+            self._res[s_name] = os.path.join(
+                self._calc_folder,self._s_cached_folder[s_name]
+                )
+        else:
+            if isinstance(val,dict) and return_stats_key in val:
+                self._stats[s_name] = val[return_stats_key]
+                self._res[s_name] = val.get(return_res_key,None)
+            else:
+                self._stats[s_name] = None
+                self._res[s_name] = val
+        return False
+    
+    def run_calcs(self):
+        """
+        Runs the calculations, provided that configuration was already loaded
+        """
+        # Initialize the collections of results and summary statistics
+        self._res = {}
+        self._stats = {}
+        for si in self._seq:
+            err = self.run_step(_str_key(si))
+            if err:
+                print("Error in step",si)
+                break
+
+        
--- readme.md
+++ readme.md
@@ -1,59 +1,65 @@
 ﻿# Configuration of calculations
 ## General concepts
-* sequence - the plan of calculations composed of steps
-* step - atomic part of calculations taking as input / producing as output
-  intermediate results
-* routine - a method / variant of implementation for a specific step; different
-  routines for a given step can be used interchangeably
-* configuration - dictionary of parameters controlling the whole process 
-  of calculations, including parameters of specific routines
+* **project** - a series of calculations based on a given set of routines where each calculation is defined by a configuration
+* **configuration** - dictionary of parameters describing the whole process 
+  of calculation, including parameters of specific routines
+* **sequence** - as a part of configuration, the plan of calculation composed of steps
+	* includes the information about dependencies between steps, where output of **parent** step(s) is/are input(s) of **child** step 
+	* in general, can be represented as an oriented graph without cycles, where nodes are steps and edges are dependencies between steps
+* routine - a method / variant of implementation for a specific step
+	* different routines implementing a same step should have the same interface ensuring interchangeability
+	* a good practice is to fix the sequence and only change routines for specific steps if needed
   
 ### Configurations
 Configurations are `dict`s stored in JSON files. 
-* Each key (parameter's name) is str. 
+* Each key (parameter's name) and each sub-key (if any) is `str`. 
 
-There are following kinds of configurations.
-* *Master* - defining the whole process of calculations
-* *Step's* - defining calculations up to the given step, i.e. step and its parents.
+There are following kinds of configuration.
+* *Master* - defining the whole process of calculation
+* *Step's* - defining calculation up to the given step, i.e. step and its parents.
 	* Contains only parameters influencing the result of this step
 	* Is stored in the folder where step's results are cached.
-* *Caching* - for a given step, configuration which defines the location of the cache folder
-	* Is obtained from step's configuration by removing irrelevant parameters, e.g. ones listed in *_invariant*
+* *Hashing* - for a given step, configuration which defines the location of the cache folder through hashing of the step\s configuration
+	* Is obtained by removing irrelevant parameters from step's configuration, e.g. ones listed in *_invariant*
 	* Is not stored explicitly, but is used for defining the cache folder name    
 ### Parameters
-There are two kinds of parameters
-* *Routines' parameters* - influencing calculations in specific routines.
-	* Can have any names, but should not start with _ and $
-* *System parameters* - related to organizing the process of calculations
-	* Have predefined names, start with _ or $
+There are two kinds of parameters in a configuration
+* *Routines' parameters* - influencing calculation in specific routines.
+	* Can have any names, which should not start with _ or $
+* *System parameters* - related to organizing the process of calculation
+	* Have predefined names, which start with _ or $
 
 ## System parameters
-* **_sequence** the calculation sequence. 
+* **_sequence** - the calculation sequence. 
 	*Format*: list of two types of elements:
 	* `str` – name of the step to execute
-	* { `str`: [list of `str`] } – name of the step to execute and list of previous steps whose outputs are its inputs
-	*Is optional*. If missing, then the step sequence is assumed to be [`dflt_step_name`], `dflt_step_name`="Main"
+	* { `str`: [list of `str`] } – name of the step to execute and list of its parent steps
+		* the order of parent steps defines the order of arguments in routine(s) implementing the child step 
+	* A child step always goes in the list after its parent steps 
+
+	*Is optional*. If `_sequence` missing, then the step sequence is assumed to be [`dflt_step_name`], where `dflt_step_name`="Main"
 	
 	
 
 * **_invariant** the list of parameters, whose values are ignored when identifying the cached folder.
 	*Format*: list of `str` (parameter names)
+	*Usage*: save time by making the caching process invariant to certain parameters, i.e. the results are not recalculated if only those parameters change 
 
-* **$step_name** for each step "*step_name*", defines the name of the routine to execute the step. format: `str`
+* **$step_name** for each step "*step_name*", defines the name of the routine to be called for executing the step. format: `str`
 
-|               | Required      | In step's config | Cache ID   |
+|               | Required      | In step's config | In hash   |
 | :-------------| :------------:|------------------|:----------:|
 | **_sequence** | no     		| Subsequence      |yes			|
 | **_invariant**| no		    | Step's + system  |no			|
 | **$step_name**| yes (for each)| For all parents  |yes			|
 
 # Routines
-For short, by a parent routine of the given routine we call the routine executing the parent step of the step executed by the given routine.
+For short, in the context of a given sequence, a *parent*/*child* routine of a given routine is a routine implementing a parent/child step of the corresponding step.
 
-Routines' names cannot start with _
+Routines' names cannot start with _ or $
 
 ## Expected behavior
-* Routine can raise any exceptions – then calculations stop, but the results from previous steps are stored.
+* Routine can raise any exceptions - then calculations stop, but the results from previous steps are stored.
 * Artificially raising an exception can be used by a routine to send a signal to stop subsequent calculations, e.g. if the results of step's calculations make no sense for proceeding to next steps, 
 
 ## Cached and non-cached routines
@@ -65,22 +71,33 @@ Cached routines are ones that save the results of calculations to a folder; non-
 `( p_1, ..., p_k, folder_name, config )`
 * `p_i` for each *i* = 1,...,*k* – output of the corresponding parent routine
 	* *k* is the number of parent routines, which can be 0 (then arguments `p_1`,...,`p_k` are missing)
-	* if the *i*-th parent routine is cached, then `p_i` is the name of the corresponding cache folder (otherwise is the returned value)
-* `folder_name` – the folder name to save output to if the routine is cached (otherwise, the argument `folder_name` is missing)  
+	* if the *i*-th parent routine is cached, then `p_i` is the name of the corresponding cache folder (otherwise is the value returned by the parent)
+* `folder_name` – the folder name to save output to, if the routine is cached (otherwise, the argument `folder_name` is missing)  
 * `config` – the corresponding step's configuration (`dict`)
 
-So, if the routine is not cached and has no parents, it has `config` as the only argument.
+So, if the routine is not cached and has no parents, it has `config` as the only argument. Moreover, if the routine does not have parameters, then it has no arguments at all.
 
 ### Routine's returned value
 
-#### For a cached routine
+Besides *the result of calculation*, a routine may produce some information as `dict`, called *summary statistics*. The purpose of the latter: display useful information such as the accuracy or calculation time. Summary statistics from all routines is collected in each calculation, and can be used to create a table summarizing the results of experiments.  
+
+#### Cached routine's returned value
 There are following options:
 1. `None`
 2. `dict` – summary statistics
 
-#### For a non-cached routine
+#### Non-cached routine's returned value
 There are following options:
-1. `dict` containing keys "*_result*" and "*_stats*" – then *_result* is the routine's output, *_stats* is `dict` containing summary statistics
+1. `dict` containing key `return_stats_key`:="*_stats*" and optionally, `return_res_key`:="*_result*" – then *_result* is the routine's output, *_stats* is `dict` containing summary statistics
 2. any other value – the routine's output
  
+## Initialization of the project
+Initialization of the project means the definition of all routines that can be used in calculations. The definition is given as a `dict` or *.json* file containing the following:
+* "routine_name": [list of names of configuration parameters the routine depends on]
+* "_cached": [list of names of cached routines] *or*
+* "_non_cached": [list of names of non-cached routines]
 
+"_cached" and "_non_cached" keys are optional:
+* if none of them presents, all the routines are considered as cached;
+* if "_cached" is given, then only the listed routines are cached, and "_non_cached" is ignored (if present)
+* if only "_non_cached" key is given, then all the routines except listed ones are considered cached
